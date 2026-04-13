cf6427ef299f88ba314a72935c5cd879f10dc367
MoamenElmalky <moamenelmalky69@gmail.com>
2026-04-13 16:42:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const button1 = document.querySelector('.Github');
+const button2 = document.querySelector('.FM');
+const button3 = document.querySelector('.LinkedIn');
+const button4 = document.querySelector('.Facebook');
+const button5 = document.querySelector('.Instagram');
+button1.addEventListener('click', () => {
+     window.open('https://github.com/MoamenElmalky',"_blank");
+})
+button2.addEventListener('click', () => {
+     window.open('https://www.frontendmentor.io/profile/moamenelmalky69-cpu','_blank');
+})
+button3.addEventListener('click', () => {
+     window.open('https://www.linkedin.com/in/moamen-elmalky-4b1093379/','_blank');
+})
+button4.addEventListener('click', () => {
+     window.open('https://www.facebook.com/moamen.m.elmalky','_blank');
+})
+button5.addEventListener('click', () => {
+     window.open('https://www.instagram.com/moamen_elmalky/','_blank');
+})
